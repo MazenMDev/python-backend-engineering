e478ce6b70ee932665c121c8ea1d538b1239dfd6
Mazen Mahmoud <mazen.m.mahmoud0@gmail.com>
2026-02-19 14:51:23 +0200

    implement 10 core list manipulation utilities

--- week-1-fundamentals/Data Structures/List Exercises.py
+++ week-1-fundamentals/Data Structures/List Exercises.py
@@ -94,3 +94,165 @@ print(remove_duplicates(["a", 'b', 'a', 'c']))
 print(get_evens([1, 2, 3, 4, 5]))
 print(alternate_merge([1,2,3], ['a','b','c']))
 print(rotate_list([1,2,3,4,5], 2))
+
+
+##################################################################################
+
+def find_second_largest(numbers):
+  seen = set()
+  for num in numbers:
+    seen.add(num)
+  if len(seen) < 2: return None
+
+  current_max = float("-inf")
+  second_max = float("-inf")
+  for num in numbers:
+    if num > current_max: current_max = num
+  for num in numbers:
+    if num < current_max and num > second_max: second_max = num
+
+  return second_max
+
+print("="*100)
+print(find_second_largest([1,2,3,4,5,6,7]))
+
+def flatten_list(nested):
+  result = []
+  for item in nested:
+    if isinstance(item, list):
+      result.extend(flatten_list(item))
+    else:
+      result.append(item)
+  return result
+
+print("HERE WE GO")
+print(flatten_list([[1, 2], [3, 4]]))          
+print(flatten_list([1, [2, [3, 4]], 5]))       
+
+
+def find_missing_number(numbers):
+  n = max(numbers)
+  total = int(n*(n+1)/2)
+  current_total = 0
+  for num in numbers:
+    current_total += num
+  missing_number = total - current_total
+  return missing_number
+
+print(find_missing_number([1,2,4,5,6]))
+
+def merge_sorted_lists(list1, list2):
+  result = []
+  i = j = 0
+
+  while i < len(list1) and j < len(list2):
+    if list1[i] <= list2[j]:
+      result.append(list1[i])
+      i+=1
+    else:
+      result.append(list2[j])
+      j+=1  
+
+  result.extend(list1[i:])
+  result.extend(list2[j:])
+
+  return result
+
+print(merge_sorted_lists([1,3,5,7,8], [2,4]))
+
+def is_palindrome(items):
+  check = True
+  for i in range(len(items)):
+    if (items[i] == items[len(items) - 1 - i]):
+      print(f"({items[i]}, {items[len(items) - 1 - i]})")
+    else: check = False
+  return check
+
+print("*"*10)
+print(is_palindrome([1,2,3,2,1]))
+
+def move_zeros_to_end(numbers):
+  zero_count = 0
+  i = 0
+  while i < len(numbers):
+    if numbers[i] == 0:
+      numbers.pop(i)
+      zero_count += 1
+    else:
+      i += 1
+
+  for _ in range(zero_count):
+    numbers.append(0)
+
+  return numbers
+
+print(move_zeros_to_end([0, 1, 0, 3, 12]))
+print(move_zeros_to_end([1, 2, 3]))
+print(move_zeros_to_end([0, 0, 1]))
+
+
+def longest_increasing_sequence(numbers):
+  if not numbers:
+    return 0  
+  
+  max_length = 1
+  current_length = 1
+
+  for i in range(1, len(numbers)):
+    if numbers[i] > numbers[i-1]:
+      current_length += 1
+      max_length = max(max_length, current_length)
+    else:
+      current_length = 1
+
+  return max_length
+
+print(longest_increasing_sequence([1, 2, 3, 1, 2, 3, 4, 5]))
+
+def remove_all(items, value):
+  while value in items:
+    items.remove(value)
+
+  return items  
+
+print("&"*100)
+print(remove_all([1,2,3,2,4,2], 2))
+print("&"*100)
+
+
+
+def find_intersection(list1, list2):
+  both = set()
+  for item1 in list1:
+    if item1 in list2:
+      both.add(item1)
+  return list(both)
+
+print(find_intersection([1, 2, 3, 4], [3, 4, 5, 6]))
+print(find_intersection([1,2,2,3], [2,2,3,4]))
+print(find_intersection([1,2], [3,4]))
+
+def rotate_left(items, k):
+  # if k > len(items) - 1 or k < 0:
+  #   k = k % len(items)
+
+  # result = []
+  # for i in range(len(items)):
+  #   if i >= k:
+  #     result.append(items[i])
+
+  # for i in range(len(items)):
+  #   if i < k:
+  #     result.append(items[i])
+
+  if not items:
+    return items
+  k = k % len(items)
+  return items[k:] + items[:k]
+
+  
+
+print(rotate_left([1,2,3,4,5], 2))
+print(rotate_left([1,2,3], 0))
+print(rotate_left([1,2,3,], 5))
+print(rotate_left([1,2,3], -1))
